c9663f81e7a4ec1373c63cddf1177d0b31ea7058
Ryan <stayrealrhr@gmail.com>
2024-06-22 00:56:00 +0800

    feat: update

--- src/tel-bot.ts
+++ src/tel-bot.ts
@@ -1,4 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
+import { tokenFromUserStart } from "./load-data";
 const token = "7176982520:AAEBxUYWqhVG0bAKGqjDd_ncLC3pNH7YNt8";
 
 const bot = new TelegramBot(token, { polling: true });
@@ -21,6 +22,30 @@ bot.onText(/\/echo (.+)/, (msg, match: any) => {
 
 bot.onText(/\/listen/, (msg, match: any) => {
   const chatId = msg.chat.id;
+  // stop = startMonitor(ids, chatId, bot);
+  // startMonitor(ids, chatId, bot);
+});
+
+const tokenList: string[] = [];
+let loading = false;
+bot.onText(/\/token_from_user (.+)/, (msg, match: any) => {
+  const chatId = msg.chat.id;
+  // const resp = match[1]; // the captured "whatever"
+  // ids.push(match[1]);
+  if (!match[1]) {
+    bot.sendMessage(chatId, "需要一个用户token");
+    return;
+  }
+  if (loading) {
+    bot.sendMessage(chatId, token + "已添加入队列中等待爬取");
+    tokenList.push(match[1]);
+  } else {
+    bot.sendMessage(chatId, token + "开始获取数据");
+    tokenList.push(match[1]);
+    tokenFromUserStart(tokenList, (token: string) => {
+      bot.sendMessage(chatId, token + "数据获取完毕");
+    });
+  }
   // stop = startMonitor(ids, chatId, bot);
   // startMonitor(ids, chatId, bot);
 });
