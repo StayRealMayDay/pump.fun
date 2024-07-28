c6033341b62dd95770237e81206b478230fa8d88
Ryan <stayrealrhr@gmail.com>
2024-07-29 02:20:21 +0800

    feat: update

--- src/task-schedule.ts
+++ src/task-schedule.ts
@@ -2,7 +2,7 @@ import schedule from "node-schedule";
 import { updateTradesAndCommentOfAllToken } from "./load-data";
 
 const updateTradeInfoSchedule = () => {
-  schedule.scheduleJob("00 00 00 * * *", () => {
+  schedule.scheduleJob("00 00 03 * * *", () => {
     updateTradesAndCommentOfAllToken();
     // console.log("hhh");
   });
